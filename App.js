b3da129ccb44c2f833cbd6c5bcd99cbee9efa469
Doradoor <167967550+Doradoor@users.noreply.github.com>
2025-10-31 17:27:41 -0600

    actualizacion

--- App.js
+++ App.js
@@ -1,22 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { estiloTextos } from './misEstilos';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+export default function MiApp() {
+  const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={estiloTextos.texto}>Hola, mundo</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 20, backgroundColor: '#fff', flex: 1 }}>
+      
+      <Text style={{ fontSize: 24, marginTop: 40, marginBottom: 30 }}>
+        Contador App
+      </Text>
+      
+      {/* CONTADOR */}
+      <Text style={{ fontSize: 18, marginTop: 20 }}>Contador:</Text>
+      <Text style={{ fontSize: 40, marginVertical: 10 }}>{contador}</Text>
+      
+      <Button title="-" onPress={() => setContador(contador - 1)} />
+      <Button title="+" onPress={() => setContador(contador + 1)} />
+      
+      {/* NOMBRE */}
+      <Text style={{ fontSize: 18, marginTop: 30 }}>Tu Nombre:</Text>
+      
+      <TextInput
+        style={{ borderWidth: 1, padding: 10, marginVertical: 10 }}
+        value={nombre}
+        onChangeText={setNombre}
+        placeholder="Escribe aquí"
+      />
+      
+      <Button title="Limpiar" onPress={() => setNombre('')} />
+      
+      {nombre && (
+        <Text style={{ marginTop: 20, fontSize: 16 }}>
+          ¡Hola, {nombre}!
+        </Text>
+      )}
+      
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
